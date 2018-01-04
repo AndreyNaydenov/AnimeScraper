e717a083ae6dfd600e4665365ee9bcc67fa0f26a
Naydenov <donute@bk.ru>
2018-01-05 00:33:08 +0600

    improved logging

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 
 if len(sys.argv) != 5:
     print("This is script that downloads pictures from zerochan.net\n" +
-          "Usage: main.py <https://www.zerochan.net/Tohsaka+Rin> <start page> <number of pages> <folder>")
+          "Usage: main.py <https://www.zerochan.net/Tohsaka+Rin> <start page> <last pages> <folder>")
     exit()
 
 main_url = sys.argv[1]
@@ -15,6 +15,6 @@ except Exception:
     print("Wrong arguments")
     exit()
 
-folder = sys.argv[4]
+folder = "images/" + sys.argv[4]
 
 scrape.main(main_url, first_page, last_page, folder)
--- scrape.py
+++ scrape.py
@@ -27,11 +27,13 @@ def save_count():
 
 def get_all_urls_on_page(main_url, number):
     urls = []
+    errors = 0
     url = main_url + "?p=" + str(number)
     page = urlopen(url).read()
     soup = BeautifulSoup(page, "html.parser")
     ul = soup.find("ul", {"id": "thumbs2"})
     if ul == None:
+        print("Page #" + str(number) + " is member only or doesn't exist.")
         return []
     li_list = ul.findChildren("li")
     for li in li_list:
@@ -39,7 +41,8 @@ def get_all_urls_on_page(main_url, number):
         try:
             urls.append(str(a['href']))
         except Exception:
-            print("! member only picture !")
+            errors += 1
+    print("Got " + str(len(urls)) + " from page #" + str(number) + ". Couldn't get " + str(errors) + " member pics.")
     return urls
 
 def download_all(urls, folder):
@@ -68,7 +71,7 @@ def main(main_url, first_page, last_page, folder_name):
 
     print("Extracting all urls...")
     urls = get_all_urls(main_url, first_page, last_page)
-    print("URLs have been extracted")
+    print("URLs have been extracted (" + str(len(urls)) + ")")
     print("Starting to download images...")
     download_all(urls, folder_name)
     print("Downloaded all")
